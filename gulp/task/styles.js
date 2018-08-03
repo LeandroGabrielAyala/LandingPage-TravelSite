ce6cb900dfdb0435007e30ad00784461c2e45e94
Leandro <lean.ayala4@gmail.com>
2018-08-02 21:16:11 -0300

    Complete footer and header generation.

--- gulp/task/styles.js
+++ gulp/task/styles.js
@@ -4,11 +4,12 @@ var gulp = require('gulp'),
  	nested = require('postcss-nested'),
  	cssimport = require('postcss-import'),
  	mixins = require('postcss-mixins'),
- 	autoprefixer = require('autoprefixer');
+ 	autoprefixer = require('autoprefixer'),
+ 	hexrgba = require('postcss-hexrgba');
 
 gulp.task('styles', function() {
 	return gulp.src('./app/assets/styles/styles.css')
-		.pipe(postcss([cssimport, mixins, cssvars, nested, autoprefixer]))
+		.pipe(postcss([cssimport, mixins, cssvars, nested, hexrgba, autoprefixer]))
 		.on('error', function(errorInfo) {
 			console.log(errorInfo.toString());
 			this.emit('end');
